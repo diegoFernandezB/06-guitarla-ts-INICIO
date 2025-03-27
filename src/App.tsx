539473be9ec3e499feaa88863d81e8ee92d28c74
Diego Fernandez <diea.fernandez@duocuc.cl>
2025-03-27 16:13:22 -0300

    pruebav1.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { cartReducer, initialState } from "./hooks/reducers/cart-reducer"
 
 function App() {
 
-  const { cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart } = useCart()
+  const {removeFromCart, decreaseQuantity, increaseQuantity, clearCart } = useCart()
 
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
